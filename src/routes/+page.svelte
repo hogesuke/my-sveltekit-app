56b9bcc5a86fc9ab99b689e628410ec5092295c4
hogesuke <miyado@gmail.com>
2024-12-15 23:05:13 +0900

    👍🏻 Tailwind CSSの導入

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,9 @@
   export let data: PageData; // load関数から渡されたデータ
 </script>
 
-<h1>SvelteKit Example</h1>
+<h1 class="text-4xl font-bold text-center text-blue-600 mt-10">
+  SvelteKit Example
+</h1>
 <p>
   メッセージ: {data.message}
 </p>
